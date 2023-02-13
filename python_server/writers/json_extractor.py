ae7f44ddbe510a4682bc8afccbd566c214ba957d
Ivan <infiwow777@yandex.ru>
2023-02-14 01:56:23 +0700

    fixed type hints for python version < 3.10

--- python_server/writers/json_extractor.py
+++ python_server/writers/json_extractor.py
@@ -1,9 +1,10 @@
 import json
+from typing import Union, Tuple
 
 
 class JSONExtractor:
     @staticmethod
-    def extract(data: bytes) -> tuple[str | None, str | None]:
+    def extract(data: bytes) -> Tuple[Union[str, None], Union[str, None]]:
         json_to_dict = json.loads(data)
         title = json_to_dict.get('title')
         url = json_to_dict.get('url')
